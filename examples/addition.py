ee2fc8c46a4b19e80bb42ab8eaa569cf427dcbc3
awsbasava6 <awsbasava02@gmail.com>
2025-07-04 19:06:00 +0530

    modified addition file

---
+++ examples/addition.py
@@ -0,0 +1,17 @@
+# addition.py
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    # Input from the user
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    # Calculate the sum
+    result = add_numbers(num1, num2)
+
+    # Display the result
+    print(f"The sum of {num1} and {num2} is {result}")
+
+print ('addition  is mathemetical function')
